1923a9a1fb4c30ae582472579b5c5b078cd89547
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-11 22:10:05 +0900

    ✅ Test: 메뉴판에 있는 메뉴인 경우 테스트 추가

--- __tests__/validators/OrderMenuValidatorTest.js
+++ __tests__/validators/OrderMenuValidatorTest.js
@@ -3,6 +3,14 @@ import OrderMenuValidator from '../../src/validators/OrderMenuValidator.js';
 
 describe('주문 메뉴와 개수 입력 예외 상황 테스트', () => {
   test.each([
+    ['양송이수프', '1'],
+    ['타파스', '2'],
+    ['바비큐립', '3'],
+  ])('메뉴판에 있는 메뉴인 경우 undefined를 반환한다.', input => {
+    expect(OrderMenuValidator.validateOrderMenu(input)).toBeUndefined();
+  });
+  
+  test.each([
     ['피자', '1'],
     ['크림파스타', '2'],
     ['데리버거', '3'],
