f7c5e033572b5696c722bd55234d6965dc841235
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-12 16:30:33 +0900

    ✅ Test: 주문 메뉴와 개수의 형식이 예시와 다른 경우 테스트 추가

--- __tests__/validators/OrderMenuValidatorTest.js
+++ __tests__/validators/OrderMenuValidatorTest.js
@@ -46,4 +46,26 @@ describe('주문 메뉴와 개수 입력 예외 상황 테스트', () => {
     };
     expect(result).toThrow(ERROR.menu.invalidOrder);
   });
+
+  test('형식이 예시와 같을 경우 undefined를 반환한다.', () => {
+    const value = [
+      ['양송이수프', '1'],
+      ['타파스', '2'],
+      ['바비큐립', '3'],
+    ];
+    const result = OrderMenuValidator.validateFormat(value);
+    expect(result).toBeUndefined();
+  });
+
+  test.each([
+    [
+      ['해산물파스타', '2'],
+      ['레드와인', '1초코케이크', '1'],
+    ],
+    [['해산물파스타', '2'], ['레드와인', '1'], ['초코케이크', '1'], ['']],
+    [['해산물파스타2'], ['레드와인', '1초코케이크', '1'], ['']],
+  ])('형식이 예시와 다른 경우 예외 처리를 한다.', input => {
+    const result = () => OrderMenuValidator.validateFormat(input);
+    expect(result).toThrow(ERROR.menu.invalidOrder);
+  });
 });
