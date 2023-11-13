7be34b0e854c4d72df8ca7214bcca1de8c511675
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-14 03:45:31 +0900

    🐞 Fix: 주문 메뉴에 따른 총 주문 금액을 계산하는 기능 테스트 코드 수정

--- __tests__/OrderMenuTest.js
+++ __tests__/OrderMenuTest.js
@@ -1,7 +1,9 @@
 import generateMenuNames from '../src/utils/generateMenuNames.js';
 import generateMenuPrices from '../src/utils/generateMenuPrices.js';
+import generateMenuCount from '../src/utils/generateMenuCount.js';
+import generateMenusInfo from '../src/utils/generateMenusInfo.js';
 import generateOrderAmountBeforeDiscount from '../src/utils/generateOrderAmountBeforeDiscount.js';
-import isAddGiveaway from '../src/utils/isAddGiveaway.js';
+import checkAddGiveaway from '../src/utils/checkAddGiveaway.js';
 
 describe('주문 기능', () => {
   test('주문 메뉴에 따른 총 주문 금액이 맞으면 금액을 반환한다.', () => {
@@ -10,38 +12,26 @@ describe('주문 기능', () => {
       ['레드와인', '1'],
       ['초코케이크', '1'],
     ];
-    const expectedValue = 110000;
-
+    const expectedValue = 145000;
     const menuNames = generateMenuNames(value);
     const menuPrices = generateMenuPrices(menuNames);
-    const result = generateOrderAmountBeforeDiscount(menuPrices);
-    expect(result).toEqual(expectedValue);
-  });
-
-  test('주문 메뉴에 따른 총 주문 금액이 맞으면 금액을 반환한다.', () => {
-    const value = [
-      ['해산물파스타', '2'],
-      ['레드와인', '1'],
-      ['초코케이크', '1'],
-    ];
-    const expectedValue = 110000;
-    const menuNames = generateMenuNames(value);
-    const menuPrices = generateMenuPrices(menuNames);
-    const result = generateOrderAmountBeforeDiscount(menuPrices);
+    const menuCount = generateMenuCount(value);
+    const orderMenusInfo = generateMenusInfo(menuNames, menuPrices, menuCount);
+    const result = generateOrderAmountBeforeDiscount(orderMenusInfo);
     expect(result).toEqual(expectedValue);
   });
 
   test('총 주문 금액에 따른 증정 메뉴가 있으면 true를 반환한다.', () => {
     const value = 120000;
     const expectedValue = true;
-    const result = isAddGiveaway(value);
+    const result = checkAddGiveaway(value);
     expect(result).toEqual(expectedValue);
   });
 
   test('총 주문 금액에 따른 증정 메뉴가 없으면 false를 반환한다.', () => {
     const value = 80000;
     const expectedValue = false;
-    const result = isAddGiveaway(value);
+    const result = checkAddGiveaway(value);
     expect(result).toEqual(expectedValue);
   });
 });
