4ba20ed7b6f647147a109190f999a840c4274d16
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-13 15:33:49 +0900

    ✅ Test: 평일 할인 금액을 계산하는 기능 테스트 추가

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -1,4 +1,6 @@
 import calculateDDayDiscount from '../src/utils/calculateDDayDiscount.js';
+import calculateWeekDayDiscount from '../src/utils/calculateWeekDayDiscount.js';
+import generateMenuNames from '../src/utils/generateMenuNames.js';
 
 describe('할인 혜택 기능 ', () => {
   test('방문 예정 날짜를 통해 크리스마스 디데이 할인 금액을 계산하여 할인금액을 반환한다.', () => {
@@ -12,6 +14,32 @@ describe('할인 혜택 기능 ', () => {
     const value = 26;
     const expectedValue = false;
     const result = calculateDDayDiscount(value);
+    expect(result).toEqual(expectedValue);
+  });
+
+  test('평일 할인 기능에서 평일 방문 날에 디저트당 할인 가격을 반환한다.', () => {
+    const value = [
+      ['해산물파스타', '2'],
+      ['아이스크림', '1'],
+      ['초코케이크', '1'],
+    ];
+    const expectedValue = -4046;
+    const menuNames = generateMenuNames(value);
+    const visitDate = 5;
+    const result = calculateWeekDayDiscount(menuNames, visitDate);
+    expect(result).toEqual(expectedValue);
+  });
+
+  test('평일 할인 기능에서 평일 방문 날이 아닌 경우 false를 반환한다.', () => {
+    const value = [
+      ['해산물파스타', '2'],
+      ['아이스크림', '1'],
+      ['초코케이크', '1'],
+    ];
+    const expectedValue = false;
+    const menuNames = generateMenuNames(value);
+    const visitDate = 2;
+    const result = calculateWeekDayDiscount(menuNames, visitDate);
     expect(result).toEqual(expectedValue);
   });
 });
