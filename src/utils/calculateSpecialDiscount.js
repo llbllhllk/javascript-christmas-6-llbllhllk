1564d53b399ea268aefe1ed7b117f1c5fa1d67e3
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-14 02:39:26 +0900

    🐞 Fix: 특별 할인 금액을 계산하는 기능 로직 수정

--- src/utils/calculateSpecialDiscount.js
+++ src/utils/calculateSpecialDiscount.js
@@ -1,8 +1,6 @@
 import CONSTANTS from '../constants/constants.js';
-import getDayOfWeek from './getDayOfWeek.js';
 
-const calculateSpecialDiscount = visitDate => {
-  const dayOfWeek = getDayOfWeek(visitDate); 
+const calculateSpecialDiscount = (visitDate, dayOfWeek) => {
   if (dayOfWeek === CONSTANTS.week.sunday || visitDate === CONSTANTS.day.christmasDay)
     return -CONSTANTS.price.specialDiscountAmount;
   return false;
